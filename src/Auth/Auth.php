0054e880151c0c0cda80aca2bc36e644a66df850
Harllei Mazetti <harlleimazetti@gmail.com>
2022-04-21 01:19:44 -0300

    Add Auth Library

--- src/Auth/Auth.php
+++ src/Auth/Auth.php
@@ -1,17 +1,39 @@
-<?php namespace Harlleimazetti\Ci4tools\Templateparser;
+<?php namespace Harlleimazetti\Ci4tools\Auth;
 
-use \Mustache_Engine as Mustache;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 
 class Auth
 {
-  protected $renderer;
-
   function __construct()
   {
-    $this->renderer = new Mustache();
+    
   }
 
-  public function render($template, $vars) {
-    return $this->renderer->render($template, $vars);
+  public function login($credentials = []) {
+    $key = getenv('app.serverKey');
+    echo "KEY: ".$key."\r\n\r\n";
+    $payload = array(
+      "iss" => "http://example.org",
+      "aud" => "http://example.com",
+      "iat" => 1356999524,
+      "nbf" => 1357000000
+    );
+
+    $jwt = $this->encodeJWT($payload, $key, 'HS256');
+    $decoded = $this->decodeJWT($jwt, new Key($key, 'HS256'));
+
+    echo $jwt;
+    print_r($decoded);
+  }
+
+  public function encodeJWT($payload, $key, $algo) {
+    $jwt = JWT::encode($payload, $key, $algo);
+    return $jwt;
+  }
+
+  public function decodeJWT($jwt, $key, $algo) {
+    $decoded = JWT::decode($jwt, new Key($key, $algo));
+    return $decoded;
   }
 }
